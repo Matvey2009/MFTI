cb9fed8046e5165efaf1510c81ec349b73318d02
Matvey <55892080+Matvey2009@users.noreply.github.com>
2024-05-27 20:01:06 +0300

    Create 1.2.4. home_data_1.2.4.py

---
+++ Курс 1/Домашнии работы/1.2.4. home_data_1.2.4.py
@@ -0,0 +1,8 @@
+a = bool(input())
+b = bool(input())
+c = bool(input())
+d = bool(input())
+if (a == True and b == True) or (a == True and c == True) and d == False:
+	print(False)
+else:
+	print(True)
