f12e7e4133ff204d065b8b7e33bfe40ec91420b2
Matvey <55892080+Matvey2009@users.noreply.github.com>
2024-08-30 20:58:42 +0300

    Create 2.4.3. class_data_2.4.3.py

---
+++ Курс 2/Классные работы/2.4.3. class_data_2.4.3.py
@@ -0,0 +1,2 @@
+n = input()
+print(n.replace('1', '0').replace('2', '1').replace('3', '2').replace('4', '3').replace('5', '4').replace('6', '5').replace('7', '6').replace('8', '7').replace('9', '8'))
